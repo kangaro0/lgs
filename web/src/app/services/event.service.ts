76a426f9823688436bf0a79b79379d454300ead8
kangaro0 <johannes_herbert@yahoo.de>
2017-09-03 14:16:14 +0200

    update

--- web/src/app/services/event.service.ts
+++ web/src/app/services/event.service.ts
@@ -11,40 +11,39 @@ import { IRestService } from '../interfaces/service.interface';
 export class EventService< IEvent, IUpdateEvent > implements IRestService< IEvent, IUpdateEvent > {
 
     private endpointURL: string = "http://localhost/lgs1/api/public/events";
+    private headers: Headers;
 
-    constructor( public http: Http ){ }
+    constructor( public http: Http ){
+        this.headers = new Headers();
+        this.headers.append( 'Access-Control-Allow-Origin', '*' );
+     }
 
     set EndpointURL( str: string ) { this.endpointURL = str; }
+    get EndpointURL() { return this.endpointURL; }
 
-    getAll( ){
-        let headers = new Headers();
-        headers.append( 'Access-Control-Allow-Origin', '*' );
-
-        return this.http.get( this.endpointURL ).flatMap( ( res: Response ) => res.json() ).map( ( item: IEvent ) => item );
+    getAll( ): Observable<IEvent> {
+        return this.http.get( this.endpointURL, { headers: this.headers } ).flatMap( ( res: Response ) => res.json() ).map( ( item: IEvent ) => item );
     }
 
     getById( id: number ): Observable<IEvent> {
-        return this.http.get( this.endpointURL + '/' + id ).map( ( res: Response ) =>  res.json() ).map( ( item: IEvent ) => item );
+        return this.http.get( this.endpointURL + '/' + id, { headers: this.headers } ).map( ( res: Response ) =>  res.json() ).map( ( item: IEvent ) => item );
     }
 
     create( item: IEvent ): Observable<Response> {
-        return this.http.post( this.endpointURL, JSON.stringify( event )).map( ( res: Response ) => res.json() );
+        return this.http.post( this.endpointURL, JSON.stringify( event ), { headers: this.headers } ).map( ( res: Response ) => res.json() );
     }
 
     update( item: IUpdateEvent ): Observable<Response> {
-        return this.http.put( this.endpointURL + '/' + item["id"], JSON.stringify( event ) ).map( ( res: Response ) => res.json() );
+        return this.http.put( this.endpointURL + '/' + item["id"], JSON.stringify( event ), { headers: this.headers } ).map( ( res: Response ) => res.json() );
     }
 
     delete( id: number ): Observable<Response> {
-        return this.http.delete( this.endpointURL + '/' + id ).map( ( res: Response ) => res.json() );
+        return this.http.delete( this.endpointURL + '/' + id, { headers: this.headers } ).map( ( res: Response ) => res.json() );
     }
 
     /* zusaetzliche Methoden, event-spezifisch */
 
     next( ): Observable<IEvent> {
-        let headers = new Headers();
-        headers.append( 'Access-Control-Allow-Origin', '*' );
-
-        return this.http.get( this.endpointURL + '/next', { headers: headers } ).map( ( res: Response ) => res.json() );
+        return this.http.get( this.endpointURL + '/next', { headers: this.headers } ).map( ( res: Response ) => res.json() );
     }
 }
